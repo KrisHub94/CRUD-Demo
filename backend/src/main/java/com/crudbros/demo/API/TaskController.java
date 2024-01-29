cf30df901f59e91dd526e5dd4c6309e182316d08
Michael <knab.codecool@gmail.com>
2024-01-29 22:01:06 +0100

    still implementing last basic crud endpoint(PUT)

--- backend/src/main/java/com/crudbros/demo/API/TaskController.java
+++ backend/src/main/java/com/crudbros/demo/API/TaskController.java
@@ -1,9 +1,11 @@
 package com.crudbros.demo.API;
 
+import com.crudbros.demo.data.Status;
 import com.crudbros.demo.model.Task;
 import com.crudbros.demo.model.TaskRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,13 +21,50 @@ public class TaskController {
     @GetMapping
     List<Task> findAllTasks() {return taskRepository.findAll();}
 
-    //TODO: TaskNotFoundException erstellen
+    //TODO: TaskNotFoundException erstellen und einfügen
     //TODO: restliche Endpoints
+    //TODO:
 
-    @GetMapping("id/{id}")
+    @GetMapping("{id}")
     Optional<Task> findById( @PathVariable long id) {
         return taskRepository.findById(id);
     }
 
+    @GetMapping("title/{title}")
+    Optional<Task> findByTitle(@PathVariable String title) {
+        return taskRepository.findByTitle(title);
+    }
+
+    @GetMapping("status/{status}")
+    List<Task> findByStatus(@PathVariable Status status) {
+        return taskRepository.findByStatus(status);
+    }
+
+    @GetMapping("start/{start}")
+    List<Task> findByStart(@PathVariable LocalDate start) {
+        return taskRepository.findByStart(start);
+    }
+
+    @GetMapping("deadline/{deadline}")
+    List<Task> findByDeadLine(@PathVariable LocalDate deadline) {
+        return taskRepository.findByDeadLine(deadline);
+    }
+
+    @PostMapping
+    Task save(@RequestBody Task newTask) {
+        return taskRepository.save(newTask);
+    }
+
+    @DeleteMapping("{id}")
+    void delete(@PathVariable long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    void updateTask(@PathVariable long id, @RequestBody Task updatedTask) {
+        Optional<Task> existingTask = taskRepository.findById(id); //hier .orElseThrow(TNFExcept.)
+
+        //TODO: TNFE, dann properties von existing Task updaten .setId etc.
+    }
 
 }
